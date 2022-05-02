725f9e418f5ffeafa3857a978d36b9395099564b
addshore <addshorewiki@gmail.com>
2022-05-02 16:41:48 +0100

    Ensure tesseract data is with the deployment
    firebase will take all the files up with it.
    So to avoid fetching the data set every time, ensure
    it is in the git repo at deploy time :)

---
+++ functions/build/fetchTesseract.js
@@ -0,0 +1,15 @@
+const Tesseract = require("tesseract.js");
+const fs = require("fs");
+
+(async () => {
+    // Force run Terreract once to ensure we have the needed data prior to deploy
+    await Tesseract.recognize(
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Wikidata-logo-en.svg/320px-Wikidata-logo-en.svg.png",
+        "eng",
+    );
+    if (!fs.existsSync("./eng.traineddata")) {
+        throw new Error("No traineddata file found");
+    } else {
+        console.log("Training data file exists!");
+    }
+})()
